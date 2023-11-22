eea95b47aaace7ef70409f208730a683975a0170
abbviksko <viktor.skold@abbgymnasiet.se>
2023-11-22 10:52:59 +0100

    removed libchess library from github by undoing commits, in the last change i added so that the code can continue from where it left off, so that we can run the code on the whole document at differant times

--- AI/EvaluateFEN.py
+++ AI/EvaluateFEN.py
@@ -1,35 +1,51 @@
 import chess
 import chess.engine
 
-count = 0
+# Function to find the number of lines in a file
+def count_lines(file_path):
+    with open(file_path, "r") as file:
+        return sum(1 for _ in file)
+
+# File paths
+positions_file = "AI/positions.txt"
+ratings_file = "AI/fen_ratings.txt"
+
+# Read already processed lines in the ratings file
+processed_rows = count_lines(ratings_file)
 
 # Open the file containing FEN strings
-with open("AI/positions.txt", "r") as file:
+with open(positions_file, "r") as file:
     fen_list = file.readlines()
 
-for fen in fen_list:
-    count += 1
-    print(f"Position {count}:")
-    print(fen.strip())  # Print the FEN string
+# Open a file to append new FEN numbers and their ratings
+with open(ratings_file, "a") as output_file:
+    for count, fen in enumerate(fen_list[processed_rows:], start=processed_rows + 1):
+        print(f"Processing position {count}...")
+        output_line = fen.strip()
 
-    # Initialize the chess board from the FEN string
-    board = chess.Board(fen.strip())
+        # Initialize the chess board from the FEN string
+        board = chess.Board(fen.strip())
 
-    # Path to the Stockfish engine executable
-    stockfish_path = "AI\stockfish\stockfish-windows-x86-64-avx2.exe"
+        # Path to the Stockfish engine executable
+        stockfish_path = "AI\stockfish\stockfish-windows-x86-64-avx2.exe"
 
-    # Create a Stockfish engine instance
-    engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
+        # Create a Stockfish engine instance
+        engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
 
-    # Get the evaluation from Stockfish for the given position
-    eval_info = engine.analyse(board, chess.engine.Limit(time=1))  # You can adjust the time limit
+        # Get the evaluation from Stockfish for the given position
+        eval_info = engine.analyse(board, chess.engine.Limit(time=1))  # You can adjust the time limit
 
-    # Adjust the evaluation score if it's Black's turn
-    if board.turn == chess.BLACK:
-        eval_info["score"].relative = -eval_info["score"].relative
+        # Adjust the evaluation score if it's Black's turn
+        if board.turn == chess.BLACK:
+            eval_info["score"].relative = -eval_info["score"].relative
 
-    # Print the evaluation score
-    print("Evaluation:", eval_info["score"])
+        # Append evaluation score to the output line
+        output_line += " " + str(eval_info["score"]) + "\n"
 
-    # Close the engine instance
-    engine.quit()
+        # Write the FEN string and its evaluation to the file
+        output_file.write(output_line)
+
+        # Close the engine instance
+        engine.quit()
+
+print("Data processing complete.")
